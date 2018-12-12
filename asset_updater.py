bd58dac7057db9d2a27f024278e147eae51a6e09
Raul Perdomo <raul@perdomo.org>
2018-12-11 22:02:28 -0500

    Updated to automatically grab the total line, also it takes the running total from the first two sheets and drops them in a third sheet which takes the values and uses them in a line chart.

--- asset_updater.py
+++ asset_updater.py
@@ -1,4 +1,4 @@
-import gspread, json
+import gspread, json, time
 from twilio.rest import Client
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -19,8 +19,13 @@ gc = gspread.authorize(credentials)
 book = gc.open('Investments')
 ws = book.get_worksheet(0)
 
+#Hoping this sleep with allow the sheet to update values.
+time.sleep(10)
+
 #Grab column C to check if it has a number of shares owned, if so add it to list of rows to gather data from.
 colC = ws.col_values(3)[1:]
+colA = ws.col_values(1)
+totalRow = colA.index('Total')+1
 index = 2
 rows_with_symbols = []
 
@@ -29,13 +34,30 @@ for row in colC:
         rows_with_symbols.append(index)
     index += 1
 
+
 #Craft Text Message
 body = 'Symbol, Change %, Gain/Loss\n'
 for row in rows_with_symbols:
     current_row = ws.row_values(row)
     body = body + current_row[0].ljust(6) + ' ' +current_row[6].rjust(4) +' '+ current_row[8]+'\n'
-body += '\n' + ws.acell('F15').value + ' ' + ws.acell('I15').value +' '+ ws.acell('G15').value
+body += '\n' + ws.acell(f'F{totalRow}').value + ' ' + ws.acell(f'I{totalRow}').value +' '+ ws.acell(f'G{totalRow}').value
+
+dayTotal = ws.acell(f'F{totalRow}').value
+sheet2totalRow = book.get_worksheet(1).col_values(1).index('Total')+1
+sheet2total = book.get_worksheet(1).acell(f'F{sheet2totalRow}').value
 print(body)
+
+#These lines create a running total on a different sheet that are grabbed to make a line chart
+ws = book.get_worksheet(2)
+rows = len(ws.col_values(1))
+print(dayTotal)
+print(float(dayTotal.strip('$').replace(',','')))
+
+ws.update_acell(f'A{rows+1}', '=today()')
+ws.update_acell(f'b{rows+1}', f'{dayTotal}')
+ws.update_acell(f'c{rows+1}', f'{sheet2total}')
+ws.update_acell(f'd{rows+1}', f"={float(dayTotal.strip('$').replace(',',''))}+{float(sheet2total.strip('$').replace(',',''))}")
+
 
 ''' Only uncomment this is you want to send texts. 
 message = client.messages.create(
